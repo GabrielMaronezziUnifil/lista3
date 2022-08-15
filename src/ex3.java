8e67fdc8c353f9fc615ffdbc1bbcc7c467c13476
GabrielMaronezziUnifil <100977293+GabrielMaronezziUnifil@users.noreply.github.com>
2022-08-15 12:51:59 -0300

    Update ex3.java

--- src/ex3.java
+++ src/ex3.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class ex3 {
+import java.util.Scanner;
+
+public class ex3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String nome;
+        String sexo;
+        int stringLength,idade,salario;
+
+
+
+        do {
+
+
+            System.out.println("Informe um nome com mais de 3 caracteres: ");
+            nome = sc.nextLine();
+            stringLength = nome.length();
+
+        }while (stringLength<3);
+
+        do {
+            System.out.println("insira uma idade entre 0 e 150:");
+            idade = sc.nextInt();
+
+        }while((idade <0)||(idade >150));
+
+        do {
+            System.out.println("Informe um salario maior que 0:");
+            salario = sc.nextInt();
+        }while(salario > 0);
+
+
+
+    }
 }
