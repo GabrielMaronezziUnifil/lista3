89a27f05a821d2785929324cdd646f9989001d96
GabrielMaronezziUnifil <100977293+GabrielMaronezziUnifil@users.noreply.github.com>
2022-08-15 12:55:32 -0300

    Update ex10.java

--- src/ex10.java
+++ src/ex10.java
@@ -1,2 +1,16 @@
+import java.util.Scanner;
+
 public class ex10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a, b,i;
+        System.out.println("Informe o inteiro A:");
+        a= sc.nextInt();
+
+        System.out.println("Informe o inteiro B:");
+        b= sc.nextInt();
+        for(i= a+1; i<b;i++){
+            System.out.print(i+ ", ");
+        }
+    }
 }
