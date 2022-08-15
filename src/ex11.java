e112e46c639cfd8407326653e9b0c49f23a4e992
GabrielMaronezziUnifil <100977293+GabrielMaronezziUnifil@users.noreply.github.com>
2022-08-15 12:56:05 -0300

    Update ex11.java

--- src/ex11.java
+++ src/ex11.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class ex11 {
+import java.util.Scanner;
+
+public class ex11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a, b,i,soma=0;
+        System.out.println("Informe o inteiro A:");
+        a= sc.nextInt();
+
+        System.out.println("Informe o inteiro B:");
+        b= sc.nextInt();
+        for(i= a+1; i<b;i++){
+            System.out.print(i+ ", ");
+            soma = soma + i;
+        }
+        System.out.println("\nA soma desses numeros e igual a :"+ soma);
+    }
 }
+
