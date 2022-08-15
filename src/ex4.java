47dbb20602bc444affd7186f603c2141d6d8fafa
GabrielMaronezziUnifil <100977293+GabrielMaronezziUnifil@users.noreply.github.com>
2022-08-15 12:52:24 -0300

    Update ex4.java

--- src/ex4.java
+++ src/ex4.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class ex4 {
+public class ex4 {
+    public static void main(String[] args) {
+        double a = 80000,b = 200000;
+        int cont=0;
+    while(b > a){
+        a = (a +(a * 0.03));
+        b = (b +(b * 0.015));
+        cont++;
+    }
+        System.out.println("Foram necessarios "+cont+" anos para a cidade A ultrapassar  cidade B");
+    }
 }
