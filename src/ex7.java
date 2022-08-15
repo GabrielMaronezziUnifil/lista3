0e7e8883cf1e8f8c6c0ac243e0c6187a90f4e561
GabrielMaronezziUnifil <100977293+GabrielMaronezziUnifil@users.noreply.github.com>
2022-08-15 12:54:39 -0300

    Update ex7.java

--- src/ex7.java
+++ src/ex7.java
@@ -1,2 +1,19 @@
+import java.util.Scanner;
+
 public class ex7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] a =  new int[5];
+        int i,m = 0;
+        for (i=0;i<5;i++){
+            System.out.println("Informe um numero inteiro: ");
+            a[i] = sc.nextInt();
+            if(m == 0){
+                m = a[i];
+            } if (a[i] > m) {
+                m = a[i];
+            }
+        }
+        System.out.println(m);
+    }
 }
