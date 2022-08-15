89713b0ec7f8fd55bad47d505a5e7725663937ee
GabrielMaronezziUnifil <100977293+GabrielMaronezziUnifil@users.noreply.github.com>
2022-08-15 12:55:00 -0300

    Update ex8.java

--- src/ex8.java
+++ src/ex8.java
@@ -1,2 +1,16 @@
+import java.util.Scanner;
+
 public class ex8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] a = new int[5];
+        float soma = 0;
+        int i;
+        for (i = 0; i < 5; i++) {
+            System.out.println("Informe um numero inteiro: ");
+            a[i] = sc.nextInt();
+            soma = soma + a[i];
+        }
+        System.out.println("A soma dos 5 numeros e: " + soma + ", E sua media e:" + soma / 5);
+    }
 }
