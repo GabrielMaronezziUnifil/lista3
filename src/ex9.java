8abd0dc1f810b3686a7ccb7ee4e616dae61288ce
GabrielMaronezziUnifil <100977293+GabrielMaronezziUnifil@users.noreply.github.com>
2022-08-15 12:55:18 -0300

    Update ex9.java

--- src/ex9.java
+++ src/ex9.java
@@ -1,2 +1,10 @@
 public class ex9 {
+    public static void main(String[] args) {
+        int i ;
+                for(i=1;i<=50;i++){
+                    if(i % 2 !=0){
+                        System.out.print(i+", ");
+                    }
+                }
+    }
 }
